f47df7a0340d3c173ea06bdd8a5e44760c07920e
JonhNino <jonhninoa@gmail.com>
2023-04-10 21:52:10 -0500

    Crud1Local2

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO agenda (id, nombre, mail, fecha,foto) VALUES(10, 'Jonh', 'Jonh@mintic.com', '2022-11-21','');
+INSERT INTO agenda (id, nombre, mail, fecha,foto) VALUES(20, 'Juan', 'Juan@gmail.com', '2022-11-21','');
+INSERT INTO agenda (id, nombre, mail, fecha,foto) VALUES(30, 'Andres', 'Andres@gmail.com', '2022-11-21','');
